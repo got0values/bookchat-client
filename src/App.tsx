7082808b8001fa0dffbe9e01cb800f7d1d04f55e
Jason Velarde <gotnovalues@gmail.com>
2023-02-19 18:31:32 -0500

    extra type import removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React,{ useState, useEffect } from 'react'
-import { Route, Routes, Navigate } from "react-router-dom";
-import { AuthContextProps, ProtectedRouteProps } from './types/types';
+import { Route, Routes } from "react-router-dom";
+import { AuthContextProps } from './types/types';
 import { ProtectedRoute } from './shared/ProtectedRoute';
 import Login from './Login';
 import Register from './Register';
