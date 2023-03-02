f9e1927c03ba203a1fb75979c1698b6149ca578f
Jason Velarde <gotnovalues@gmail.com>
2023-03-02 03:43:04 -0500

    commento test

--- src/Commento.tsx
+++ src/Commento.tsx
@@ -1,14 +1,29 @@
-import { Box, Heading } from "@chakra-ui/react"
+import { useEffect } from 'react';
+import { Box, Heading } from "@chakra-ui/react";
+
 
 export const Commento = ({server}: {server: string}) => {
+
+  let scriptElement: HTMLScriptElement = document.createElement("script");
+  scriptElement.id = "scriptelement"
+  scriptElement.src = "https://commento.communitybookclub.com/js/commento.js";
+  scriptElement.async = true;
+
+  useEffect(()=>{
+    let documentScriptElement = document.getElementById("scriptelement");
+    if (!documentScriptElement){
+      document.body.appendChild(scriptElement);
+    }
+    return ()=>{
+      document.body.removeChild(scriptElement)
+      documentScriptElement = null;
+    }
+  },[])
 
   return (
     <Box>
       <Heading as="h1" size="xl" mb={5}>Commento</Heading>
-      <Box>
-        <script defer src="https://commento.communitybookclub.com/js/commento.js"></script>
-        <div id="commento"></div>
-      </Box>
+      <Box id="commento"></Box>
     </Box>
   )
 }
