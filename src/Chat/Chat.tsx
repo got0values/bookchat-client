da18e8a717b1c942d6243ae87e63f8792a709021
Jason Velarde <gotnovalues@gmail.com>
2023-05-30 20:53:45 -0400

    chat search

--- src/Chat/Chat.tsx
+++ src/Chat/Chat.tsx
@@ -1,8 +1,6 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { ActiveRoom } from "../types/types";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { DashboardProps, Following_Following_self_profile_idToProfile, CurrentlyReading, CurrentlyReadingComment } from '../types/types';
 import { 
   Box,
   Heading,
@@ -14,27 +12,27 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Avatar,
-  Portal,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
+  Image,
+  Tabs, 
+  TabList, 
+  TabPanels, 
+  Tab, 
+  TabPanel,
   IconButton,
+  Stack,
   useColorMode,
   useToast
 } from "@chakra-ui/react";
-import { useAuth } from '../hooks/useAuth';
-import { BsReplyFill, BsEmojiSmile } from 'react-icons/bs';
-import { FiSend } from "react-icons/fi";
-import Picker from '@emoji-mart/react';
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import {socket} from "./customSocket";
+import axios from "axios";
 
 
 export default function Chat() {
   dayjs.extend(utc);
   const navigate = useNavigate();
+  const {colorMode} = useColorMode();
 
   function disconnectSocket() {
     socket.disconnect();
@@ -73,76 +71,187 @@ export default function Chat() {
     })
   },[socket])
 
+  const searchChatRoomRef = useRef({} as HTMLInputElement);
+  const [chatSearchIsLoading,setChatSearchIsLoading] = useState(false);
+  const [chatRoomResults,setChatRoomResults] = useState<any[] | null>(null);
+  async function searchChatRoom() {
+    setChatSearchIsLoading(true)
+    await axios
+      .get("https://www.googleapis.com/books/v1/volumes?q=" + searchChatRoomRef.current.value)
+      .then((response)=>{
+        console.log(response.data.items)
+        setChatRoomResults(response.data.items)
+        setChatSearchIsLoading(false)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+  }
+
   return (
     <>
       <Box className="main-content-smaller">
         <Skeleton isLoaded={true}>
-          <Box
-            className="well"
+          <Tabs 
+            variant="enclosed"
+            p={2}
           >
-            <Heading size="md" mb={2}>Active</Heading>
-            <Flex
-              direction="column"
-              gap={2}
+            <TabList
+              borderBottom="none"
             >
-            {activeRooms.length ? (
-              activeRooms.map((room,i,arr)=>{
-                return (
-                  <Flex 
-                    className="well-card"
-                    justify="space-between"
-                    _hover={{
-                      cursor: "pointer"
-                    }}
-                    onClick={e=>navigate(`/chat/room?title=${room?.bookTitle ? room.bookTitle : ""}&author=${room?.bookAuthor ? room.bookAuthor : ""}`)}
-                    key={i}
-                  >
-                    <Flex
-                      gap={1}
-                      wrap="wrap"
-                    >
-                      <Text fontStyle="italic">{room?.bookTitle ? room?.bookTitle : ""} </Text>
-                      <Text>{room?.bookAuthor ? room?.bookAuthor : ""}</Text>
-                    </Flex>
-                    <Text>
-                      {room?.numberOfUsers ? room.numberOfUsers : "0"}
-                    </Text>
-                  </Flex>
-                )
-              })
-            ) : (
-              <Heading
-                as="h4"
-                size="sm"
-                fontStyle="italic"
+              <Tab 
+                _selected={{
+                  bg: colorMode === "light" ? "white" : "#121212",
+                  border: colorMode === "light" ? "1px solid #e7e7e7" : "1px solid #2e2e2e",
+                  borderBottom: "none"
+                }}
               >
-                No Rooms Active
-              </Heading>
-            )}
-            </Flex>
-          </Box>
+                Search
+              </Tab>
+              <Tab 
+                _selected={{
+                  bg: colorMode === "light" ? "white" : "#121212",
+                  border: colorMode === "light" ? "1px solid #e7e7e7" : "1px solid #2e2e2e",
+                  borderBottom: "none"
+                }}
+              >
+                Active
+              </Tab>
+            </TabList>
+
+            <TabPanels
+              bg="white"
+              rounded="md"
+              border="1px solid #e7e7e7"
+              _dark={{
+                bg: "#121212",
+                border: "1px solid #2e2e2e"
+              }}
+              // minH="80vh"
+            >
+
+              <TabPanel>
+
+                <Flex
+                  direction="column"
+                  gap={2}
+                >
+                  <Flex gap={1}>
+                    <Input 
+                      type="text"
+                      ref={searchChatRoomRef}
+                      onKeyDown={e=>e.key === 'Enter' ? searchChatRoom() : null}
+                    />
+                    <Button
+                      onClick={e=>searchChatRoom()}
+                    >
+                      Search
+                    </Button>
+                  </Flex>
+                  {chatRoomResults ? (
+                    <Flex
+                      className="well"
+                      wrap="wrap"
+                      justify="space-between"
+                      gap={3}
+                    >
+                      {chatRoomResults?.map((result)=>{
+                        return (
+                          <Flex
+                            direction="column"
+                            gap={1}
+                            maxW="150px"
+                            flex="1 1 50%"
+                          >
+                            <Image 
+                              w="100%"
+                              h="auto"
+                              m={1}
+                              className="book-image"
+                              onError={(e)=>(e.target as HTMLImageElement).src = "https://via.placeholder.com/165x215"}
+                              src={result.volumeInfo.imageLinks ? result.volumeInfo.imageLinks.smallThumbnail : null}
+                            />
+                            <Text
+                              size="sm"
+                              fontStyle="italic"
+                              textOverflow="wrap"
+                            >
+                              {result.volumeInfo.title ? result.volumeInfo.title : null}
+                            </Text>
+                            <Text
+                              size="sm"
+                              textOverflow="wrap"
+                            >
+                              {result.volumeInfo.authors ? result.volumeInfo.authors[0] : null}
+                            </Text>
+                            <Button
+                              size="xs"
+                              onClick={e=>navigate(`/chat/room?title=${result.volumeInfo.title ? result.volumeInfo.title : ""}&author=${result.volumeInfo.authors ? result.volumeInfo.authors[0] : ""}`)}
+                            >
+                              Chat
+                            </Button>
+                          </Flex>
+                        )
+                      })}
+                    </Flex>
+                  ) : null}
+                </Flex>
+
+              </TabPanel>
+              <TabPanel>
+
+                <Box
+                  className="well"
+                >
+                  <Heading size="md" mb={2}>Active</Heading>
+                  <Flex
+                    direction="column"
+                    gap={2}
+                  >
+                  {activeRooms.length ? (
+                    activeRooms.map((room,i,arr)=>{
+                      return (
+                        <Flex 
+                          className="well-card"
+                          justify="space-between"
+                          _hover={{
+                            cursor: "pointer"
+                          }}
+                          onClick={e=>navigate(`/chat/room?title=${room?.bookTitle ? room.bookTitle : ""}&author=${room?.bookAuthor ? room.bookAuthor : ""}`)}
+                          key={i}
+                        >
+                          <Flex
+                            gap={1}
+                            wrap="wrap"
+                          >
+                            <Text fontStyle="italic">{room?.bookTitle ? room?.bookTitle : ""} </Text>
+                            <Text>{room?.bookAuthor ? room?.bookAuthor : ""}</Text>
+                          </Flex>
+                          <Text>
+                            {room?.numberOfUsers ? room.numberOfUsers : "0"}
+                          </Text>
+                        </Flex>
+                      )
+                    })
+                  ) : (
+                    <Heading
+                      as="h4"
+                      size="sm"
+                      fontStyle="italic"
+                    >
+                      No Rooms Active
+                    </Heading>
+                  )}
+                  </Flex>
+                </Box>
+
+              </TabPanel>
+
+            </TabPanels>
+
+          </Tabs>
         </Skeleton>
       </Box>
-
-      {/* <Modal 
-        isOpen={isOpenCommentModal} 
-        onClose={closeCommentModal}
-        isCentered
-      >
-        <ModalOverlay />
-        <ModalContent maxH="80vh">
-          <ModalHeader>
-            
-          </ModalHeader>
-          <ModalCloseButton />
-            <ModalBody h="auto" maxH="75vh" overflow="auto">
-
-            </ModalBody>
-            <ModalFooter flexDirection="column">
-
-            </ModalFooter>
-        </ModalContent>
-      </Modal> */}
 
     </>
   );
