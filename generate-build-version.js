2afca93e33f7764fde70b4507bfed6ab533c83a7
Jason Velarde <gotnovalues@gmail.com>
2023-07-11 03:39:50 -0400

    build

--- generate-build-version.js
+++ generate-build-version.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import packageJson from'./package.json';
+import fs from "fs";
+import packageJson from "./package.json" assert { type: "json" };
 
 const appVersion = packageJson.version;
 
