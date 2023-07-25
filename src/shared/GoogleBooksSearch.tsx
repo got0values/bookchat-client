5958ee57bbd65bd94b7f01a4759641a114654426
Jason Velarde <gotnovalues@gmail.com>
2023-07-25 02:04:41 -0400

    fixed first book club book

--- src/shared/GoogleBooksSearch.tsx
+++ src/shared/GoogleBooksSearch.tsx
@@ -38,7 +38,6 @@ export default function GoogleBooksSearch({selectText,selectCallback,gBooksApi}:
       // .get("https://openlibrary.org/sear" + searchInputRef.current.value)
       .then((response)=>{
         if (response.data.items) {
-          console.log(response.data.items)
           setBookResultsGoogle(response.data.items)
         }
         else {
