7d6eccc2d0097c8947a596b6f1024710d9e7e0a5
Jason Velarde <gotnovalues@gmail.com>
2023-08-31 00:06:38 -0400

    isloading

--- src/Book Suggestions/BookSuggestToList.tsx
+++ src/Book Suggestions/BookSuggestToList.tsx
@@ -16,7 +16,7 @@ import {
   PopoverBody,
   PopoverArrow,
   Divider,
-  Skeleton,
+  Spinner,
 } from "@chakra-ui/react";
 import { SuggestionCountBadge } from "../shared/SuggestionCount";
 import { FaPlay, FaArrowCircleRight } from 'react-icons/fa'
@@ -66,6 +66,13 @@ export function BookSuggestionToList({server}: {server: string;}) {
     }
   })
 
+  if (isLoading) {
+    return (
+      <Flex align="center" justify="center" minH="90vh">
+        <Spinner size="xl"/>
+      </Flex>
+    )
+  }
   if (isError) {
     return <Flex align="center" justify="center" minH="90vh">
       <Heading as="h1" size="xl">Error: {(error as Error).message}</Heading>
@@ -73,146 +80,145 @@ export function BookSuggestionToList({server}: {server: string;}) {
   }
 
   return (
-    <Skeleton
-      isLoaded={!isLoading}
-    >
-      {firstBookshelf ? (
-        <Flex
-          align="center"
-          justify="center"
-          wrap="wrap"
-          gap={2}
-          // mb={3}
-        >
-          <Button
-            as="a"
-            href={`/booksuggestions/bookshelf?profile=${firstBookshelf.Profile.username}`}
-            // variant="outline"
-            colorScheme="teal"
-            size="sm"
-            aria-label="random bookshelf"
-            display="flex"
-            gap={2}
-            // borderColor="purple"
-            // p={2}
-            mb={1}
-          >
-            Random <FaPlay size={15}/>
-          </Button>
-        </Flex>
-      ): null}
-      {bookSuggestToList?.length ? (
-        bookSuggestToList.map((bookshelf: BookshelfType, i: number)=>{
-          return (
-            <React.Fragment key={i}>
-              <Box
-                // className="well"
+    <>
+        <>
+          {firstBookshelf ? (
+            <Flex
+              align="center"
+              justify="center"
+              wrap="wrap"
+              gap={2}
+              // mb={3}
+            >
+              <Button
+                as="a"
+                href={`/booksuggestions/bookshelf?profile=${firstBookshelf.Profile.username}`}
+                // variant="outline"
+                colorScheme="teal"
+                size="sm"
+                aria-label="random bookshelf"
+                display="flex"
+                gap={2}
+                // borderColor="purple"
+                // p={2}
+                mb={1}
               >
-                <Flex 
-                  align="flex-start"
-                  justify="space-between"
-                >
-                  <Flex
-                    align="center"
-                    gap={2}
-                    wrap="wrap"
+                Random <FaPlay size={15}/>
+              </Button>
+            </Flex>
+          ): null}
+          {bookSuggestToList?.length ? (
+            bookSuggestToList.map((bookshelf: BookshelfType, i: number)=>{
+              return (
+                <React.Fragment key={i}>
+                  <Box
+                    // className="well"
                   >
-                    <Avatar 
-                      as={Link}
-                      to={`/profile/${bookshelf.Profile.username}`}
-                      src={bookshelf.Profile.profile_photo} 
-                      size="sm"
-                      // name={bookshelf.Profile.username}
-                    />
-                    <Flex align="center" gap={1}>
-                      <Text 
-                        fontWeight="bold"
-                        fontSize="sm"
-                        as={Link}
-                        to={`/profile/${bookshelf.Profile.username}`}
-                      >
-                        @{bookshelf.Profile.username}
-                      </Text>
-                      {/* <Box w="1.4rem">
-                        {bookshelf.Flag ? <bookshelf.Flag/> : null}
-                      </Box> */}
-                      <SuggestionCountBadge suggestionCount={bookshelf.Profile._count.BookSuggestion_BookSuggestion_suggestorToProfile}/>
-                    </Flex>
-                  </Flex>
-                  <Flex
-                    align="center"
-                    justify="space-between"
-                    gap={2}
-                    width="125px"
-                  >
-                    <Box>
-                      <Box fontSize="sm">
-                        <Text as="span" fontWeight="bold">Shelf:</Text> {(bookshelf as any)._count.BookshelfBook}
-                      </Box>
-                      {bookshelf.start_poll ? (
-                        <Text 
-                          fontSize="sm"
-                          fontWeight="bold"
-                          color="green"
-                        >
-                          Poll
-                        </Text>
-                      ): null}
-                    </Box>
-                    <Button
-                      as={Link}
-                      to={`/booksuggestions/bookshelf?profile=${bookshelf.Profile.username}`}
-                      variant="ghost"
-                      p={0}
+                    <Flex 
+                      align="flex-start"
+                      justify="space-between"
                     >
-                      <FaArrowCircleRight size={20} color="teal" />
-                    </Button>
-                  </Flex>
-                </Flex>
-                <Box>
-                  <Popover isLazy>
-                    <PopoverTrigger>
-                      <Box
-                        _hover={{
-                          cursor: "pointer"
-                        }}
+                      <Flex
+                        align="center"
+                        gap={2}
+                        wrap="wrap"
                       >
-                        {bookshelf.suggestions_notes ? (
-                          <Text fontStyle="italic" noOfLines={2}>
-                            "{bookshelf.suggestions_notes}"
+                        <Avatar 
+                          as={Link}
+                          to={`/profile/${bookshelf.Profile.username}`}
+                          src={bookshelf.Profile.profile_photo} 
+                          size="sm"
+                          // name={bookshelf.Profile.username}
+                        />
+                        <Flex align="center" gap={1}>
+                          <Text 
+                            fontWeight="bold"
+                            fontSize="sm"
+                            as={Link}
+                            to={`/profile/${bookshelf.Profile.username}`}
+                          >
+                            @{bookshelf.Profile.username}
                           </Text>
-                        ): null}
-                      </Box>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                      <PopoverArrow />
-                      <PopoverCloseButton />
-                      <PopoverBody 
-                        fontSize="sm"
-                        _dark={{
-                          bg: "black"
-                        }}
+                          {/* <Box w="1.4rem">
+                            {bookshelf.Flag ? <bookshelf.Flag/> : null}
+                          </Box> */}
+                          <SuggestionCountBadge suggestionCount={bookshelf.Profile._count.BookSuggestion_BookSuggestion_suggestorToProfile}/>
+                        </Flex>
+                      </Flex>
+                      <Flex
+                        align="center"
+                        justify="space-between"
+                        gap={2}
+                        width="125px"
                       >
-                        {bookshelf.suggestions_notes}
-                      </PopoverBody>
-                    </PopoverContent>
-                  </Popover>
-                </Box>
-              </Box>
-              {i !== bookSuggestToList.length - 1 ? (
-                <Divider borderColor="blackAlpha.600" my={2} />
-              ): null}
-            </React.Fragment>
-          )
-        })
-      ): (
-        <Box>
-          <Text fontStyle="italic">
-            Please check back tomorrow!
-          </Text>
-        </Box>
-      )}
-
-    </Skeleton>
+                        <Box>
+                          <Box fontSize="sm">
+                            <Text as="span" fontWeight="bold">Shelf:</Text> {(bookshelf as any)._count.BookshelfBook}
+                          </Box>
+                          {bookshelf.start_poll ? (
+                            <Text 
+                              fontSize="sm"
+                              fontWeight="bold"
+                              color="green"
+                            >
+                              Poll
+                            </Text>
+                          ): null}
+                        </Box>
+                        <Button
+                          as={Link}
+                          to={`/booksuggestions/bookshelf?profile=${bookshelf.Profile.username}`}
+                          variant="ghost"
+                          p={0}
+                        >
+                          <FaArrowCircleRight size={20} color="teal" />
+                        </Button>
+                      </Flex>
+                    </Flex>
+                    <Box>
+                      <Popover isLazy>
+                        <PopoverTrigger>
+                          <Box
+                            _hover={{
+                              cursor: "pointer"
+                            }}
+                          >
+                            {bookshelf.suggestions_notes ? (
+                              <Text fontStyle="italic" noOfLines={2}>
+                                "{bookshelf.suggestions_notes}"
+                              </Text>
+                            ): null}
+                          </Box>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                          <PopoverArrow />
+                          <PopoverCloseButton />
+                          <PopoverBody 
+                            fontSize="sm"
+                            _dark={{
+                              bg: "black"
+                            }}
+                          >
+                            {bookshelf.suggestions_notes}
+                          </PopoverBody>
+                        </PopoverContent>
+                      </Popover>
+                    </Box>
+                  </Box>
+                  {i !== bookSuggestToList.length - 1 ? (
+                    <Divider borderColor="blackAlpha.600" my={2} />
+                  ): null}
+                </React.Fragment>
+              )
+            })
+          ): (
+            <Box>
+              <Text fontStyle="italic">
+                Please check back tomorrow!
+              </Text>
+            </Box>
+          )}
+        </>
+    </>
   )
 }
--- src/Stats/Leaderboard.tsx
+++ src/Stats/Leaderboard.tsx
@@ -20,7 +20,8 @@ import {
   PopoverCloseButton,
   PopoverContent,
   PopoverBody,
-  PopoverArrow
+  PopoverArrow,
+  Spinner
 } from "@chakra-ui/react";
 import { ImInfo } from 'react-icons/im';
 import Cookies from "js-cookie";
@@ -56,8 +57,10 @@ export default function Leaderboard({server}: {server: string}) {
   const [followingPagesReadLeaderboard,setFollowingPagesReadLeaderboard] = useState<any[]>([]);
   const [allPointsLeaderboard,setAllPointsLeaderboard] = useState<any[]>([]);
   const [followingPointsLeaderboard,setFollowingPointsLeaderboard] = useState<any[]>([]);
+  const [isLoading,setIsLoading] = useState(false);
   async function getLeaderboard() {
     const tokenCookie = Cookies.get().token
+    setIsLoading(true)
     await axios
       .get(`${server}/api/leaderboard`,
       {
@@ -76,6 +79,7 @@ export default function Leaderboard({server}: {server: string}) {
         console.log(response)
         throw new Error(response.message)
       })
+      setIsLoading(false)
   }
 
   useEffect(()=>{
@@ -137,211 +141,219 @@ export default function Leaderboard({server}: {server: string}) {
   
   return (
     <Box>
-      <Text fontWeight="bold" className="non-well" textAlign="center">
-        {dayjs(thisWeekStart).format('M/D/YY')} - {dayjs(thisWeekStart).add(6,"day").format('M/D/YY')}
-      </Text>
-      <Heading as="h2" size="lg" className="non-well">All Users</Heading>
-      <Box className="well">
-        <Flex gap={2} align="center">
-          <Heading as="h3" size="md" mb={2} >Points</Heading>
-          <PointsPopover/>
+      {isLoading ? (
+        <Flex align="center" justify="center" minH="80vh">
+          <Spinner size="xl"/>
         </Flex>
-        <TableContainer>
-          <Table variant='simple' size="sm">
-            <Thead>
-              <Tr>
-                <Th>Pos.</Th>
-                <Th>Username</Th>
-                <Th>Score</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {allPointsLeaderboard && allPointsLeaderboard.length ? (
-                allPointsLeaderboard.map((ap,i)=>{
-                  return (
-                    <Tr key={i}>
-                      <Td>
-                        {i + 1}
-                      </Td>
-                      <Td>
-                        <Flex 
-                          as={Link}
-                          to={`/profile/${ap.username}`}
-                          align="center" 
-                          gap={1}
-                        >
-                          <Avatar
-                            size="xs"
-                            src={`${ap.profile_photo}`}
-                            border="2px solid gray"
-                            name={`${ap.username}`}
-                          >
-                          </Avatar>
-                          <Text>
-                            @{ap.username}
-                          </Text>
-                        </Flex>
-                      </Td>
-                      <Td>{ap.totalScore}</Td>
-                    </Tr>
-                  )
-                })
-              ): null}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-      {/* <Box className="well">
-        <Heading as="h3" size="md" mb={2} >Pages Read</Heading>
-        <TableContainer>
-          <Table variant='simple' size="sm">
-            <Thead>
-              <Tr>
-                <Th>Pos.</Th>
-                <Th>Username</Th>
-                <Th>Pages Read</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {allPagesReadLeaderboard && allPagesReadLeaderboard.length ? (
-                allPagesReadLeaderboard.map((apr,i)=>{
-                  return (
-                    <Tr key={i}>
-                      <Td>
-                        {i + 1}
-                      </Td>
-                      <Td>
-                        <Flex 
-                          as={Link}
-                          to={`/profile/${apr.Profile.username}`}
-                          align="center" 
-                          gap={1}
-                        >
-                          <Avatar
-                            size="xs"
-                            src={`${apr.Profile.profile_photo}`}
-                            border="2px solid gray"
-                            name={`${apr.Profile.username}`}
-                          >
-                          </Avatar>
-                          <Text>
-                            @{apr.Profile.username}
-                          </Text>
-                        </Flex>
-                      </Td>
-                      <Td>{apr.read}</Td>
-                    </Tr>
-                  )
-                })
-              ): null}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box> */}
-      <Divider mb={1} mt={4} />
-      <Heading as="h2" size="lg" className="non-well">Following</Heading>
-      <Box 
-        className="well" 
-        sx={{
-          marginBottom: ".5rem!important"
-        }}
-      >
-        <Flex gap={2} align="center">
-          <Heading as="h3" size="md" mb={2} >Points</Heading>
-          <PointsPopover/>
-        </Flex>
-        <TableContainer>
-          <Table variant='simple' size="sm">
-            <Thead>
-              <Tr>
-                <Th>Pos.</Th>
-                <Th>Username</Th>
-                <Th>Score</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {followingPointsLeaderboard && followingPointsLeaderboard.length ? (
-                followingPointsLeaderboard.map((fp,i)=>{
-                  return (
-                    <Tr key={i}>
-                      <Td>
-                        {i + 1}
-                      </Td>
-                      <Td>
-                        <Flex 
-                          as={Link}
-                          to={`/profile/${fp.username}`}
-                          align="center" 
-                          gap={1}
-                        >
-                          <Avatar
-                            size="xs"
-                            src={`${fp.profile_photo}`}
-                            border="2px solid gray"
-                            name={`${fp.username}`}
-                          >
-                          </Avatar>
-                          <Text>
-                            @{fp.username}
-                          </Text>
-                        </Flex>
-                      </Td>
-                      <Td>{fp.totalScore}</Td>
-                    </Tr>
-                  )
-                })
-              ): null}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-      <Box className="well">
-        <Heading as="h3" size="md" mb={2} >Pages Read</Heading>
-        <TableContainer>
-          <Table variant='simple' size="sm">
-            <Thead>
-              <Tr>
-                <Th>Pos.</Th>
-                <Th>Username</Th>
-                <Th>Pages Read</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {followingPagesReadLeaderboard && followingPagesReadLeaderboard.length ? (
-                followingPagesReadLeaderboard.map((fpr,i)=>{
-                  return (
-                    <Tr key={i}>
-                      <Td>
-                        {i + 1}
-                      </Td>
-                      <Td>
-                        <Flex 
-                          as={Link}
-                          to={`/profile/${fpr.Profile.username}`}
-                          align="center" 
-                          gap={1} 
-                        >
-                          <Avatar
-                            size="xs"
-                            src={`${fpr.Profile.profile_photo}`}
-                            border="2px solid gray"
-                            name={`${fpr.Profile.username}`}
-                          >
-                          </Avatar>
-                          <Text>
-                            @{fpr.Profile.username}
-                          </Text>
-                        </Flex>
-                      </Td>
-                      <Td>{fpr.read}</Td>
-                    </Tr>
-                  )
-                })
-              ): null}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
+      ): (
+        <>
+          <Text fontWeight="bold" className="non-well" textAlign="center">
+            {dayjs(thisWeekStart).format('M/D/YY')} - {dayjs(thisWeekStart).add(6,"day").format('M/D/YY')}
+          </Text>
+          <Heading as="h2" size="lg" className="non-well">All Users</Heading>
+          <Box className="well">
+            <Flex gap={2} align="center">
+              <Heading as="h3" size="md" mb={2} >Points</Heading>
+              <PointsPopover/>
+            </Flex>
+            <TableContainer>
+              <Table variant='simple' size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>Pos.</Th>
+                    <Th>Username</Th>
+                    <Th>Score</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {allPointsLeaderboard && allPointsLeaderboard.length ? (
+                    allPointsLeaderboard.map((ap,i)=>{
+                      return (
+                        <Tr key={i}>
+                          <Td>
+                            {i + 1}
+                          </Td>
+                          <Td>
+                            <Flex 
+                              as={Link}
+                              to={`/profile/${ap.username}`}
+                              align="center" 
+                              gap={1}
+                            >
+                              <Avatar
+                                size="xs"
+                                src={`${ap.profile_photo}`}
+                                border="2px solid gray"
+                                name={`${ap.username}`}
+                              >
+                              </Avatar>
+                              <Text>
+                                @{ap.username}
+                              </Text>
+                            </Flex>
+                          </Td>
+                          <Td>{ap.totalScore}</Td>
+                        </Tr>
+                      )
+                    })
+                  ): null}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Box>
+          {/* <Box className="well">
+            <Heading as="h3" size="md" mb={2} >Pages Read</Heading>
+            <TableContainer>
+              <Table variant='simple' size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>Pos.</Th>
+                    <Th>Username</Th>
+                    <Th>Pages Read</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {allPagesReadLeaderboard && allPagesReadLeaderboard.length ? (
+                    allPagesReadLeaderboard.map((apr,i)=>{
+                      return (
+                        <Tr key={i}>
+                          <Td>
+                            {i + 1}
+                          </Td>
+                          <Td>
+                            <Flex 
+                              as={Link}
+                              to={`/profile/${apr.Profile.username}`}
+                              align="center" 
+                              gap={1}
+                            >
+                              <Avatar
+                                size="xs"
+                                src={`${apr.Profile.profile_photo}`}
+                                border="2px solid gray"
+                                name={`${apr.Profile.username}`}
+                              >
+                              </Avatar>
+                              <Text>
+                                @{apr.Profile.username}
+                              </Text>
+                            </Flex>
+                          </Td>
+                          <Td>{apr.read}</Td>
+                        </Tr>
+                      )
+                    })
+                  ): null}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Box> */}
+          <Divider mb={1} mt={4} />
+          <Heading as="h2" size="lg" className="non-well">Following</Heading>
+          <Box 
+            className="well" 
+            sx={{
+              marginBottom: ".5rem!important"
+            }}
+          >
+            <Flex gap={2} align="center">
+              <Heading as="h3" size="md" mb={2} >Points</Heading>
+              <PointsPopover/>
+            </Flex>
+            <TableContainer>
+              <Table variant='simple' size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>Pos.</Th>
+                    <Th>Username</Th>
+                    <Th>Score</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {followingPointsLeaderboard && followingPointsLeaderboard.length ? (
+                    followingPointsLeaderboard.map((fp,i)=>{
+                      return (
+                        <Tr key={i}>
+                          <Td>
+                            {i + 1}
+                          </Td>
+                          <Td>
+                            <Flex 
+                              as={Link}
+                              to={`/profile/${fp.username}`}
+                              align="center" 
+                              gap={1}
+                            >
+                              <Avatar
+                                size="xs"
+                                src={`${fp.profile_photo}`}
+                                border="2px solid gray"
+                                name={`${fp.username}`}
+                              >
+                              </Avatar>
+                              <Text>
+                                @{fp.username}
+                              </Text>
+                            </Flex>
+                          </Td>
+                          <Td>{fp.totalScore}</Td>
+                        </Tr>
+                      )
+                    })
+                  ): null}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Box>
+          <Box className="well">
+            <Heading as="h3" size="md" mb={2} >Pages Read</Heading>
+            <TableContainer>
+              <Table variant='simple' size="sm">
+                <Thead>
+                  <Tr>
+                    <Th>Pos.</Th>
+                    <Th>Username</Th>
+                    <Th>Pages Read</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {followingPagesReadLeaderboard && followingPagesReadLeaderboard.length ? (
+                    followingPagesReadLeaderboard.map((fpr,i)=>{
+                      return (
+                        <Tr key={i}>
+                          <Td>
+                            {i + 1}
+                          </Td>
+                          <Td>
+                            <Flex 
+                              as={Link}
+                              to={`/profile/${fpr.Profile.username}`}
+                              align="center" 
+                              gap={1} 
+                            >
+                              <Avatar
+                                size="xs"
+                                src={`${fpr.Profile.profile_photo}`}
+                                border="2px solid gray"
+                                name={`${fpr.Profile.username}`}
+                              >
+                              </Avatar>
+                              <Text>
+                                @{fpr.Profile.username}
+                              </Text>
+                            </Flex>
+                          </Td>
+                          <Td>{fpr.read}</Td>
+                        </Tr>
+                      )
+                    })
+                  ): null}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Box>
+        </>
+      )}
     </Box>
   )
 }
