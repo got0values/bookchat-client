fbff31c008f236b5180a3049445e0c8fe9941f8c
Jason Velarde <gotnovalues@gmail.com>
2023-09-07 23:47:44 -0400

    favicon build

--- src/AdminArea.tsx
+++ src/AdminArea.tsx
@@ -114,7 +114,7 @@ export default function AdminArea({server}: AdminAreaProps) {
                   colorScheme="green"
                   onClick={e=>createRegistrationLink()}
                 >
-                  Save
+                  Submit
                 </Button>
               </Flex>
             </Flex>
